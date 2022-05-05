70ca3c68bfc05e1a4c84476e520aebe25d07ef0c
winnifrede.wang <winnifrede.wang@vipshop.com>
2022-05-06 00:24:58 +0800

    feat: 1.增加login文件;2.增加styles;3.修复文件名必须mult-words的格式问题

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     intent: 0,
-    'space-before-function-paren': 0
+    'space-before-function-paren': 0,
+    'vue/multi-word-component-names': 'off'
   }
 }
--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,4 @@
 <template>
-  <div class="box"></div>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
   <router-view />
   <!-- <el-row class="mb-4">
     <el-button>Default</el-button>
@@ -15,33 +10,5 @@
     <el-button>中文</el-button>
   </el-row> -->
 </template>
-<script setup>
-const boxWidth = '100px'
-</script>
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-.box {
-  width: v-bind(boxWidth);
-  height: 100px;
-  background-color: rebeccapurple;
-}
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<script setup></script>
+<style lang="scss"></style>
