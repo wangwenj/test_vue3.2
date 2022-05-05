dc09596b530293e2dc0487ee6bbfdbe2701ee13e
winnifrede.wang <winnifrede.wang@vipshop.com>
2022-05-05 18:07:32 +0800

    feat: fix: 测试element按需导入是否成功

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,16 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <router-view />
+  <el-row class="mb-4">
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+    <el-button>中文</el-button>
+  </el-row>
 </template>
 
 <style lang="scss">
