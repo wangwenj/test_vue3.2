dc4ae7fa8e40541117071aca4b9b40ed3b4dcd3a
winnifrede.wang <winnifrede.wang@vipshop.com>
2022-05-05 18:17:51 +0800

    test: 测试vue3.2的新功能:style能够绑定变量

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
+  <div class="box"></div>
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
   <router-view />
-  <el-row class="mb-4">
+  <!-- <el-row class="mb-4">
     <el-button>Default</el-button>
     <el-button type="primary">Primary</el-button>
     <el-button type="success">Success</el-button>
@@ -12,9 +13,11 @@
     <el-button type="warning">Warning</el-button>
     <el-button type="danger">Danger</el-button>
     <el-button>中文</el-button>
-  </el-row>
+  </el-row> -->
 </template>
-
+<script setup>
+const boxWidth = '100px'
+</script>
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -24,6 +27,11 @@
   color: #2c3e50;
 }
 
+.box {
+  width: v-bind(boxWidth);
+  height: 100px;
+  background-color: rebeccapurple;
+}
 nav {
   padding: 30px;
 
